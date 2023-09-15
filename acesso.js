0a926014097a83a8151d3565028cc269e2af1b8d
estherramoss <estherramosbocchini@gmail.com>
2023-09-15 14:54:09 -0300

    Adição

---
+++ acesso.js
@@ -0,0 +1,56 @@
+const btn = document.querySelector("#btn")
+
+const usuario = document.querySelector(".usuario").value;
+
+const senha = document.querySelector(".senha").value;
+
+
+function send(){
+
+const usuario = document.querySelector(".usuario").value;
+
+const senha = document.querySelector(".senha").value;
+
+const preencha = document.querySelector("#preencha")
+
+const sucesso = document.querySelector("#sucesso")
+
+
+if(usuario.length <1 || senha.length <1){
+    
+    preencha.style.display = ('inline')
+
+    setTimeout(() => {
+
+        preencha.style.display = ('none')
+    
+        }, 1000);
+
+    return false;
+}
+
+if(usuario === "Bruno" && senha === "130306"){
+
+
+    sucesso.style.display = ('inline')
+
+    setTimeout(() => {
+
+    location.href = "http://127.0.0.1:5500/home.html";
+
+    }, 500);
+    
+} else{
+
+    negado.style.display = ('inline')
+
+    setTimeout(() => {
+
+        location.href = "http://127.0.0.1:5500/erro.html";
+    
+        }, 500);
+
+};
+
+
+};
